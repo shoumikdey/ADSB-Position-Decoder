577be5cb6d57b576c5336a9bd031183f68d20953
Shoumik Dey <shoumikdey123@gmail.com>
2018-11-10 15:11:38 +0530

    TC added for msgs with DF=17

--- adsb_freq.py
+++ adsb_freq.py
@@ -7,13 +7,24 @@ def df(frame):
     df = int(bin_frame[0:5], 2)
     return df
 
+def getTC(frame):
+    #frame = "8D40621D58C382D690C8AC2863A7"
+    data = frame[8:22]
+    bin = hexToDec(data)
+    return int(bin[0:5],2)
+
 fhandle = open('adsb_20181108_0.txt')
 fr = dict()
 for line in fhandle:
     fr[line[1:len(line)-2]] = fr.get(line[1:len(line)-2], 0) + 1
 dict = list()
 for hex, freq in fr.items():
-    dict.append((hex, df(hex), freq))
+    if df(hex) == 17:
+        dict.append((hex, df(hex), getTC(hex), freq))
+    else:
+        dict.append((hex, df(hex), "NIL", freq))
 
-for frame_hex, df_hex, frequency in dict:
-    print("Frame:", frame_hex, "DF:", df_hex, "frequency: ", frequency)
+
+for frame_hex, df_hex, tc, frequency in dict:
+    if df_hex == 17:
+        print("Frame:", frame_hex, "DF:", df_hex, "TC:", tc, "frequency: ", frequency)
